15e725ebbb3832936b1249d8c3a389cad8f9997b
jordicido <jordi.cidoncha4@gmail.com>
2026-03-17 17:21:36 +0100

    Modificación sesion 3

--- sesion3/ejercicio8.py
+++ sesion3/ejercicio8.py
@@ -3,4 +3,3 @@ nombre2 = input("Indica otro nombre: ")
 nombre3 = input("Indica otro nombre: ")
 
 print(f"Alguien es Juan? {nombre1 == "Juan" or nombre2 == "Juan" or nombre3 == "Juan"}")
-print(f"Alguien es Juan? {"Juan" in [nombre1, nombre2, nombre3]}")
